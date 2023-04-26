299e39375b9bd1fd4da052569a3cad3f521a7ac1
RK-0396 <rk96.prajapati@gmail.com>
2023-04-26 14:05:55 +0530

    Added employee commit

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class Employee {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
